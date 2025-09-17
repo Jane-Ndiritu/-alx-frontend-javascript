a7306195be04b51bf1af4ca9bf0aa0c307031c73
jane <jayndiritu36@gmail.com>
2025-09-17 15:02:36 +0300

    contains the function createEmployee:

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -108,10 +108,10 @@ class Teacher implements TeacherInterface {
   }
   }
 
-  function createEmployee (salary: number | string): string {
-    if( typeof salary === "number" && salary < 500) {
-      return  "Teacher";
+  function createEmployee (salary: number): Director | Teacher {
+    if( salary < 500) {
+      return new Teacher();
     } else {
-      return  "Director";
+      return new Director();
     }
   }
